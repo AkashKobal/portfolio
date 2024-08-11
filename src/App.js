5abad93a7c29414114fd8f04d5087769af7f65fb
Akash <akashkobal02@gmail.com>
2024-08-11 22:32:44 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
 import { darkTheme, lightTheme } from './utils/Themes.js'
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Mayur Nangre</Logo>
+        <Logo>Akash Kobal</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Mayur Nangre. All rights reserved.
+          &copy; 2024 Akash Kobal. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
